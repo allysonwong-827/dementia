b7bf7c9cb1df90b88d628390d13ef309a7ff7ab1
AllysonW <allyscwong@gmail.com>
2022-08-27 22:37:56 +0800

    Initial Commit

---
+++ dementia/dementiaApp.swift
@@ -0,0 +1,20 @@
+//
+//  dementiaApp.swift
+//  dementia
+//
+//  Created by Allyson Wong on 27/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct dementiaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
